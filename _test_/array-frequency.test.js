f5429a2fc19ee38aa6bf11f4d3ccc6f1d4617542
niloaydin <nilay.aydn97@gmail.com>
2022-02-27 02:02:32 +0300

    Change the name to array-frequency and add more tests

---
+++ _test_/array-frequency.test.js
@@ -0,0 +1,33 @@
+const frequencyOfArray = require("../index");
+
+it("Should get the frequency of the element in an array", () => {
+  const freqOfArray2 = frequencyOfArray([1, 1, 2, 3, 3, 3, 4]);
+  expect(freqOfArray2).toEqual({ 4: 1, 1: 2, 2: 1, 3: 3 });
+});
+
+it("Should get the frequency of the stringed elements in an array", () => {
+  const freqOfArray3 = frequencyOfArray(["hi", "how", "are", "you", "?"]);
+  expect(freqOfArray3).toEqual({ hi: 1, how: 1, are: 1, you: 1, "?": 1 });
+});
+
+it("Should get the frequency of the stringed elements in an array", () => {
+  const freqOfArray4 = frequencyOfArray(["hi how are you?"]);
+  expect(freqOfArray4).toEqual({ "hi how are you?": 1 });
+});
+
+it("Should throw an error when it's not given an array", () => {
+  const freqOfArray5 = frequencyOfArray(1234);
+  expect(() => {
+    freqOfArray5;
+  }).toThrow(new Error("You need to pass an array"));
+});
+
+// it("Should throw an error when it's not given an array", () => {
+//   const freqOfString6 = frequencyOfArray([null]);
+//   expect(freqOfString6).toEqual(undefined);
+// });
+
+// it("Should throw an error when it's not given an array", () => {
+//   const freqOfString7 = frequencyOfArray([undefined]);
+//   expect(freqOfString7).toThrow(new Error("You need to pass an array"));
+// });
