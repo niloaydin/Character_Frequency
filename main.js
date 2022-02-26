5f628d0b0987ab270d6efdd1db3b8218250e5d1a
niloaydin <nilay.aydn97@gmail.com>
2022-02-27 02:03:00 +0300

    Create main.js for string frequency

---
+++ main.js
@@ -0,0 +1,16 @@
+function frequencyOfString(message) {
+  const output = {};
+  let arr = message.toLowerCase().split("");
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+    if (output[word] === undefined) {
+      output[word] = 1;
+    } else {
+      output[word] += 1;
+    }
+  }
+  return output;
+}
+
+module.exports = frequencyOfString;
+//
