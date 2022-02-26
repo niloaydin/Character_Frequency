87621068a088d06ae4396fe4e602f4c35b117e3a
niloaydin <nilay.aydn97@gmail.com>
2022-02-26 18:39:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 function frequency(message) {
-  if (typeof message === "string") {
-    let output = {};
-    let arr = message.toLowerCase().split("");
-    for (let i = 0; i < arr.length; i++) {
-      let word = arr[i];
-      if (output[word] === undefined) {
-        output[word] = 1;
-      } else {
-        output[word] += 1;
-      }
-    }
-    return output;
-  }
   if (Array.isArray(message) === true) {
     const map = {};
     message.forEach((item) => {
@@ -24,3 +11,7 @@ function frequency(message) {
     return map;
   }
 }
+
+console.log(frequency([12234234234, 24423423, 2342342]));
+
+module.exports = frequency;
