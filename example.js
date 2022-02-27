97b01b3077edbfa65d5f508cd1db34e2fad95a22
niloaydin <nilay.aydn97@gmail.com>
2022-02-27 16:42:47 +0300

    Add an example.js to see if codes are working

---
+++ example.js
@@ -0,0 +1,10 @@
+const frequencyOfArray = require("./index");
+const frequencyOfString = require("./index2");
+
+const result = frequencyOfArray([1, 2, 3, 4, 4, 4, 4, 5, 5, 5, 9]);
+
+console.log(result);
+
+const result2 = frequencyOfString("Banana and APpLe");
+
+console.log(result2);
