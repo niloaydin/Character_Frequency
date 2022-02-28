c8c2fcb057c47a4415d9043b1824e9d8bd9ed438
niloaydin <nilay.aydn97@gmail.com>
2022-02-28 12:42:00 +0300

    Extend the test

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -1,4 +1,4 @@
-const frequencyOfArray = require("../index");
+const { frequencyOfArray, frequencyOfString } = require("../index");
 
 it("Should get the frequency of the element in an array", () => {
   const freqOfArray2 = frequencyOfArray([1, 1, 2, 3, 3, 3, 4]);
@@ -26,4 +26,52 @@ it("Should throw an error when it's not given a parameter", () => {
   expect(() => {
     frequencyOfArray();
   }).toThrow(new Error("You need to pass an array"));
+});
+
+it("Should get the frequency of the element of the string", () => {
+  const freqOfString2 = frequencyOfString("banana");
+  expect(freqOfString2).toEqual({ b: 1, a: 3, n: 2 });
+});
+
+it("Should get the frequency of the element of the string", () => {
+  const freqOfString3 = frequencyOfString("banana");
+  expect(freqOfString3).toEqual({ a: 3, n: 2, b: 1 });
+});
+
+it("Should get the frequency of the element of the string", () => {
+  const freqOfString4 = frequencyOfString("112223333");
+  expect(freqOfString4).toEqual({ 1: 2, 2: 3, 3: 4 });
+});
+
+it("Should get the frequency of the spaces and speacial characters of the string", () => {
+  const freqOfString5 = frequencyOfString("hello, ** ()  ");
+  expect(freqOfString5).toEqual({
+    h: 1,
+    e: 1,
+    l: 2,
+    o: 1,
+    ",": 1,
+    " ": 4,
+    "*": 2,
+    "(": 1,
+    ")": 1,
+  });
+});
+
+it("Should get the frequency of the element of the string and should be case insensitive", () => {
+  const freqOfString6 = frequencyOfString("BaNanA");
+  expect(freqOfString6).toEqual({ b: 1, a: 3, n: 2 });
+});
+
+it("Should throw an error when the parameter is not a string", () => {
+  const hello = null;
+  expect(() => {
+    frequencyOfString(hello);
+  }).toThrow(Error);
+});
+
+it("Should throw an error when no parameter is passed", () => {
+  expect(() => {
+    frequencyOfString();
+  }).toThrow(Error);
 });
