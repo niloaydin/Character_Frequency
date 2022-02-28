c0ef606e4f21b1197d51f9a637a13242dd5f41db
niloaydin <nilay.aydn97@gmail.com>
2022-02-28 12:41:46 +0300

    add index2.js to index.js and make them one file

--- index.js
+++ index.js
@@ -14,4 +14,18 @@ function frequencyOfArray(message) {
   }
 }
 
-module.exports = frequencyOfArray;
+function frequencyOfString(message) {
+  const output = {};
+  let arr = message.toLowerCase().split("");
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+    if (output[word] === undefined) {
+      output[word] = 1;
+    } else {
+      output[word] += 1;
+    }
+  }
+  return output;
+}
+
+module.exports = { frequencyOfArray, frequencyOfString };
